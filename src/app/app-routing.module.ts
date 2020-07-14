34a1a77092a009b11e894f9cafbc56671fa9310d
JHades <jhades.dev@gmail.com>
2020-07-14 12:41:10 +0200

    Angular Universal Course

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,27 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HomeComponent} from "./home/home.component";
+import {CourseComponent} from "./course/course.component";
+import {CourseResolver} from "./services/course.resolver";
 
+const routes: Routes = [
+    {
+        path: "",
+        component: HomeComponent
 
-const routes: Routes = [];
+    },
+    {
+        path: 'courses/:id',
+        component: CourseComponent,
+        resolve: {
+            course: CourseResolver
+        }
+    },
+    {
+        path: "**",
+        redirectTo: '/'
+    }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
